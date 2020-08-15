57c2ab51ad874ec3e71de0706b67d25dcddc28a3
Shuric110 <65159520+Shuric110@users.noreply.github.com>
2020-08-15 18:11:20 +0300

    Закрытие редактора по Esc, сообщение при отсутствии точек, рефакторинг

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import EventView from "./view/event.js";
 import FilterView from "./view/filter.js";
 import MenuView from "./view/menu.js";
 import SortView from "./view/sort.js";
+import TripEventsView from "./view/trip-events.js";
+import NoEventsView from "./view/no-events.js";
 import TripDayListView from "./view/trip-day-list.js";
 import TripDayView from "./view/trip-day.js";
 import TripInfoView from "./view/trip-info.js";
@@ -19,12 +21,18 @@ const journeySummary = createJourneySummary(events);
 
 const tripMainElement = document.querySelector(`.trip-main`);
 const tripControlsElement = tripMainElement.querySelector(`.trip-controls`);
-const tripEventsElement = document.querySelector(`.trip-events`);
+const tripMainContainerElement = document.querySelector(`main .page-body__container`);
 
-const renderEvent = function (evt) {
+const renderEvent = function (eventListElement, evt) {
   const eventComponent = new EventView(evt);
   let eventEditorComponent;
 
+  const onEscKeyDown = function (keyboardEvt) {
+    if (keyboardEvt.key === `Escape` || keyboardEvt.key === `Esc`) {
+      keyboardEvt.preventDefault();
+      switchToView();
+    }
+  };
 
   const switchToEdit = function () {
     if (!eventEditorComponent) {
@@ -35,13 +43,19 @@ const renderEvent = function (evt) {
         switchToView();
       });
 
+      eventEditorComponent.getElement().querySelector(`.event__rollup-btn`).addEventListener(`click`, function () {
+        switchToView();
+      });
+
       eventComponent.getElement().parentNode.replaceChild(eventEditorComponent.getElement(), eventComponent.getElement());
+      document.addEventListener(`keydown`, onEscKeyDown);
     }
   };
 
   const switchToView = function () {
     eventEditorComponent.getElement().parentNode.replaceChild(eventComponent.getElement(), eventEditorComponent.getElement());
     eventEditorComponent = null;
+    document.removeEventListener(`keydown`, onEscKeyDown);
   };
 
   eventComponent.getElement().querySelector(`.event__rollup-btn`).addEventListener(`click`, function () {
@@ -51,29 +65,39 @@ const renderEvent = function (evt) {
   render(eventListElement, eventComponent.getElement(), RenderPosition.BEFOREEND);
 };
 
+const renderTripEvents = function (tripEventsContainer, tripEventsList) {
+  const tripEventsElement = new TripEventsView().getElement();
+  render(tripEventsContainer, tripEventsElement, RenderPosition.BEFOREEND);
+
+  if (tripEventsList.length === 0) {
+    render(tripEventsElement, new NoEventsView().getElement(), RenderPosition.BEFOREEND);
+    return;
+  }
+  render(tripEventsElement, new SortView().getElement(), RenderPosition.BEFOREEND);
+
+  const tripDayListElement = new TripDayListView().getElement();
+  render(tripEventsElement, tripDayListElement, RenderPosition.BEFOREEND);
+
+  let currentDate = ``;
+  let currentDateNumber = 0;
+  let eventListElement;
+
+  for (let evt of tripEventsList) {
+    const eventDate = formatDateAsDateMD(evt.beginDateTime);
+    if (eventDate !== currentDate) {
+      currentDate = eventDate;
+      currentDateNumber++;
+      const tripDayElement = new TripDayView(currentDate, currentDateNumber).getElement();
+      render(tripDayListElement, tripDayElement, RenderPosition.BEFOREEND);
+      eventListElement = tripDayElement.querySelector(`.trip-events__list`);
+    }
+
+    renderEvent(eventListElement, evt);
+  }
+};
 
 render(tripMainElement, new TripInfoView(journeySummary).getElement(), RenderPosition.AFTERBEGIN);
 render(tripControlsElement, new FilterView().getElement(), RenderPosition.BEFOREEND);
 render(tripControlsElement, new MenuView().getElement(), RenderPosition.BEFOREEND);
 
-render(tripEventsElement, new SortView().getElement(), RenderPosition.BEFOREEND);
-render(tripEventsElement, new TripDayListView().getElement(), RenderPosition.BEFOREEND);
-
-const tripDayListElement = tripEventsElement.querySelector(`.trip-days`);
-
-let currentDate = ``;
-let currentDateNumber = 0;
-let eventListElement;
-
-for (let evt of events) {
-  const eventDate = formatDateAsDateMD(evt.beginDateTime);
-  if (eventDate !== currentDate) {
-    currentDate = eventDate;
-    currentDateNumber++;
-    render(tripDayListElement, new TripDayView(currentDate, currentDateNumber).getElement(), RenderPosition.BEFOREEND);
-    const eventListElements = tripDayListElement.querySelectorAll(`.trip-events__list`);
-    eventListElement = eventListElements[eventListElements.length - 1];
-  }
-
-  renderEvent(evt);
-}
+renderTripEvents(tripMainContainerElement, events);
