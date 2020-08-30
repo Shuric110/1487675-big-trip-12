72db2c83495b501806e78d5ef2fe91c90ac48dbf
Shuric110 <65159520+Shuric110@users.noreply.github.com>
2020-08-30 15:55:44 +0300

    Фильтрация

--- src/main.js
+++ src/main.js
@@ -2,10 +2,10 @@ import BoardModel from "./model/board.js";
 import EventsModel from "./model/events.js";
 import DestinationModel from "./model/destination.js";
 
-import FilterView from "./view/filter.js";
 import MenuView from "./view/menu.js";
 import TripInfoView from "./view/trip-info.js";
 
+import FilterPresenter from "./presenter/filter.js";
 import TripPresenter from "./presenter/trip.js";
 
 import {RenderPosition, render} from "./util/render.js";
@@ -33,11 +33,11 @@ const tripMainElement = document.querySelector(`.trip-main`);
 const tripControlsElement = tripMainElement.querySelector(`.trip-controls`);
 const tripMainContainerElement = document.querySelector(`main .page-body__container`);
 
+const filterPresenter = new FilterPresenter(tripControlsElement, boardModel);
 const tripPresenter = new TripPresenter(tripMainContainerElement, eventsModel, destinationModel, boardModel);
-
 
 render(tripMainElement, new TripInfoView(journeySummary), RenderPosition.AFTERBEGIN);
 render(tripControlsElement, new MenuView(), RenderPosition.BEFOREEND);
-render(tripControlsElement, new FilterView(), RenderPosition.BEFOREEND);
 
+filterPresenter.init();
 tripPresenter.init(events);
--- src/view/filter.js
+++ src/view/filter.js
@@ -1,12 +1,38 @@
 import ComponentView from "./component.js";
 
 export default class Filter extends ComponentView {
+  constructor(filterDefinitions, currentFilter) {
+    super();
+    this._filterDefinitions = filterDefinitions;
+    this._currentFilter = currentFilter;
+
+    this._filterTypeChangeHandler = this._filterTypeChangeHandler.bind(this);
+  }
+
+  _filterTypeChangeHandler(evt) {
+    evt.preventDefault();
+    this._callback.filterTypeChange(evt.target.value);
+  }
+
+  setFilterTypeChangeHandler(callback) {
+    this._callback.filterTypeChange = callback;
+    this.getElement().addEventListener(`change`, this._filterTypeChangeHandler);
+  }
+
   getTemplate() {
     return `
       <div>
         <h2 class="visually-hidden">Filter events</h2>
         <form class="trip-filters" action="#" method="get">
-          <div class="trip-filters__filter">
+          ${this._filterDefinitions.map(({code, title}) => `
+            <div class="trip-filters__filter">
+              <input id="filter-${code}" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter"
+                value="${code}" ${code === this._currentFilter.code ? `checked` : ``}
+              >
+              <label class="trip-filters__filter-label" for="filter-${code}">${title}</label>
+            </div>
+          `).join(``)}
+<!--          <div class="trip-filters__filter">
             <input id="filter-everything" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="everything" checked>
             <label class="trip-filters__filter-label" for="filter-everything">Everything</label>
           </div>
@@ -20,7 +46,7 @@ export default class Filter extends ComponentView {
             <input id="filter-past" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="past">
             <label class="trip-filters__filter-label" for="filter-past">Past</label>
           </div>
-
+-->
           <button class="visually-hidden" type="submit">Accept filter</button>
         </form>
       </div>
